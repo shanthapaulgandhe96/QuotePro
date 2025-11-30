59030b659c591abc702aa2ec971d90e32bd4a1fa
Shanthapaul Gandhe <Shanthapaulgandhe96@gmail.com>
2025-11-30 12:00:00 +0530

    Implemented Firebase Firestore for save NewQuote

--- app/src/main/java/uk/ac/tees/mad/quotepro/data/repo/NewQuoteRepoImpl.kt
+++ app/src/main/java/uk/ac/tees/mad/quotepro/data/repo/NewQuoteRepoImpl.kt
@@ -1,28 +1,46 @@
 package uk.ac.tees.mad.quotepro.data.repo
 
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.tasks.await
+import uk.ac.tees.mad.quotepro.data.local.dao.QuoteDao
 import uk.ac.tees.mad.quotepro.data.local.entity.QuoteEntity
 import uk.ac.tees.mad.quotepro.domain.repo.NewQuoteRepo
+import uk.ac.tees.mad.quotepro.utils.Constants
+import javax.inject.Inject
 
-class NewQuoteRepoImpl : NewQuoteRepo {
+class NewQuoteRepoImpl @Inject constructor(
+    private val quoteDao: QuoteDao,
+    private val firestore: FirebaseFirestore,
+) : NewQuoteRepo {
+
     override suspend fun insertQuote(quote: QuoteEntity) {
-        TODO("Not yet implemented")
+        quoteDao.insertQuote(quote = quote)
+        firestore.collection(Constants.QUOTES_COLLECTION)
+            .document(quote.id)
+            .set(quote).await()
     }
 
     override suspend fun updateQuote(quote: QuoteEntity) {
-        TODO("Not yet implemented")
+        quoteDao.updateQuote(quote = quote)
+        firestore.collection(Constants.QUOTES_COLLECTION)
+            .document(quote.id)
+            .set(quote).await()
     }
 
     override suspend fun deleteQuote(quote: QuoteEntity) {
-        TODO("Not yet implemented")
+        quoteDao.deleteQuote(quote = quote)
+        firestore.collection(Constants.QUOTES_COLLECTION)
+            .document(quote.id)
+            .delete().await()
     }
 
     override suspend fun getQuoteById(quoteId: String): QuoteEntity? {
-        TODO("Not yet implemented")
+        return quoteDao.getQuoteById(quoteId)
     }
 
     override fun getAllQuotes(userId: String): Flow<List<QuoteEntity>> {
-        TODO("Not yet implemented")
+        return quoteDao.getAllQuotes(userId)
     }
 
 }
